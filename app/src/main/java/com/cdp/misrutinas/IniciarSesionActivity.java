0ece14bc8efa0231252320876f47e4a5f1e08fa7
VeroTorres <verofxianeca@gmail.com>
2023-09-23 18:28:07 -0300

    creación de activity inicar sesión

---
+++ app/src/main/java/com/cdp/misrutinas/IniciarSesionActivity.java
@@ -0,0 +1,14 @@
+package com.cdp.misrutinas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IniciarSesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+    }
+}
