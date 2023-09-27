e2d6c50a6d9b3ceec1e6e3ce4922d9b0911e269b
fede9087 <fede9087@gmail.com>
2023-09-27 15:10:25 -0300

    Agrego menu al dashboard + navegabilidad de login a dashboard

--- app/src/main/java/com/cdp/misrutinas/DashboardActivity.java
+++ app/src/main/java/com/cdp/misrutinas/DashboardActivity.java
@@ -1,14 +1,41 @@
 package com.cdp.misrutinas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    BottomNavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        nav = findViewById(R.id.nav);
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    Toast.makeText(DashboardActivity.this, "Inicio", Toast.LENGTH_LONG).show();
+                } else if (itemId == R.id.finanza) {
+                    Toast.makeText(DashboardActivity.this, "Finanza", Toast.LENGTH_LONG).show();
+                } else if (itemId == R.id.calendario) {
+                    Toast.makeText(DashboardActivity.this, "Calendario", Toast.LENGTH_LONG).show();
+                } else if (itemId == R.id.contacto) {
+                    Toast.makeText(DashboardActivity.this, "Contacto", Toast.LENGTH_LONG).show();
+                }
+
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/cdp/misrutinas/IniciarSesionActivity.java
+++ app/src/main/java/com/cdp/misrutinas/IniciarSesionActivity.java
@@ -2,7 +2,9 @@ package com.cdp.misrutinas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IniciarSesionActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class IniciarSesionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+    }
+    public void btnIngresar(View view){
+        Intent intent=new Intent(IniciarSesionActivity.this,DashboardActivity.class);
+        startActivity(intent);
     }
 }
