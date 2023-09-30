aeae344fe1f0008948bdc961984260e88c590aec
leonelrasjido18 <leonelrasjido17ispc@gmail.com>
2023-09-30 15:17:17 -0300

    agrego activitys lista de socios y de profesores

---
+++ app/src/main/java/com/cdp/misrutinas/ListaProfesorActivity.java
@@ -0,0 +1,14 @@
+package com.cdp.misrutinas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaProfesorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_profesor);
+    }
+}
