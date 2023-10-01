f3fcf61a77ff8c6bc099c08e3bd971b32888219d
VeroTorres <verofxianeca@gmail.com>
2023-09-30 23:01:30 -0300

    Activity Registro terminada + navegabilidad

--- app/src/main/java/com/cdp/misrutinas/RegistroActivity.java
+++ app/src/main/java/com/cdp/misrutinas/RegistroActivity.java
@@ -2,7 +2,9 @@ package com.cdp.misrutinas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+
+    public void btnVolverRegistro(View view){
+        Intent intent=new Intent(RegistroActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }
