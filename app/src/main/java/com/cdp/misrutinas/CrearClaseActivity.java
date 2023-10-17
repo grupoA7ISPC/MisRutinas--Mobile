508233ef548349208cf64cad6ecb451782c709fd
VeroTorres <verofxianeca@gmail.com>
2023-10-16 22:31:21 -0300

    Activities creadas Editar Clase y Crear Clase

---
+++ app/src/main/java/com/cdp/misrutinas/CrearClaseActivity.java
@@ -0,0 +1,14 @@
+package com.cdp.misrutinas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearClaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_clase);
+    }
+}
