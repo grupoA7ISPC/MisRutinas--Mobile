07eabac13461aed49daf469ab21f3a0d1b6251f2
estebanmartins <estebanmartins25@gmail.com>
2023-10-26 18:17:45 -0300

    se soluciona bug de crearEntrenadorActivity

--- app/src/main/java/com/cdp/misrutinas/CrearEntrenadorActivity.java
+++ app/src/main/java/com/cdp/misrutinas/CrearEntrenadorActivity.java
@@ -28,6 +28,37 @@ public class CrearEntrenadorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_entrenador);
+
+        textNombre = findViewById(R.id.textNombre);
+        textApellido = findViewById(R.id.textApellido);
+        textDNI = findViewById(R.id.textDNI);
+        textEmail = findViewById(R.id.textEmail);
+        textPhone = findViewById(R.id.textPhone);
+
+        btnGuardar = findViewById(R.id.btnGuardar);
+
+        textoResultado = findViewById(R.id.textoResultado);
+
+        crud = new CrudCliente(this);
+
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = textNombre.getText().toString();
+                String apellido = textApellido.getText().toString();
+                String dni = textDNI.getText().toString();
+                String email = textEmail.getText().toString();
+                String telefono = textPhone.getText().toString();
+
+                long id = crud.insertarCliente(nombre, apellido, dni, email, telefono, idRol);
+
+                if (id != -1) {
+                    textoResultado.setText("Registro insertado con ID: " + id);
+                } else {
+                    textoResultado.setText("Error al insertar el registro.");
+                }
+            }
+        });
     }
 
     public void btnVolverListaEntrenador(View view){
