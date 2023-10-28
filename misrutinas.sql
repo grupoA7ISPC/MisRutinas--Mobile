363d0e91504829dcf21a8ca5f70fdbd3a986b7ad
estebanmartins <estebanmartins25@gmail.com>
2023-10-28 17:58:08 -0300

    se actualiza script.sql y modeloRelacional y se sincroniza segun el proyecto

--- misrutinas.sql
+++ misrutinas.sql
@@ -14,11 +14,11 @@ CREATE SCHEMA IF NOT EXISTS `MisRutinasApp` DEFAULT CHARACTER SET utf8 ;
 USE `MisRutinasApp` ;
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`rol`
+-- Table `MisRutinasApp`.`Rol`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`rol` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Rol` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`rol` (
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Rol` (
   `id_rol` INT NOT NULL AUTO_INCREMENT,
   `nombre_rol` CHAR(10) NOT NULL,
   PRIMARY KEY (`id_rol`),
@@ -27,106 +27,104 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`usuario`
+-- Table `MisRutinasApp`.`Usuario`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`usuario` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Usuario` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`usuario` (
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Usuario` (
   `id_usuario` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(20) NULL,
   `apellido` VARCHAR(45) NULL,
   `nombre` VARCHAR(45) NULL,
   `dni` INT NULL,
-  `fec_nac` DATE NULL,
-  `email` VARCHAR(45) NULL,
-  `pass` VARCHAR(45) NULL,
-  `active` TINYINT NULL,
-  `usuariocol` VARCHAR(45) NULL,
-  `fk_rol` INT NOT NULL,
-  PRIMARY KEY (`id_usuario`, `fk_rol`),
+  `email` VARCHAR(75) NOT NULL,
+  `pass` VARCHAR(16) NULL,
+  `tel` INT NULL,
+  `id_rol` INT NOT NULL,
+  PRIMARY KEY (`id_usuario`, `id_rol`),
   UNIQUE INDEX `id_socio_UNIQUE` (`id_usuario` ASC) VISIBLE,
-  INDEX `fk_usuario_rol1_idx` (`fk_rol` ASC) VISIBLE,
+  INDEX `fk_usuario_rol1_idx` (`id_rol` ASC) VISIBLE,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
   CONSTRAINT `fk_usuario_rol1`
-    FOREIGN KEY (`fk_rol`)
-    REFERENCES `MisRutinasApp`.`rol` (`id_rol`)
+    FOREIGN KEY (`id_rol`)
+    REFERENCES `MisRutinasApp`.`Rol` (`id_rol`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`socio`
+-- Table `MisRutinasApp`.`Socio`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`socio` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Socio` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`socio` (
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Socio` (
   `id_socio` INT NOT NULL AUTO_INCREMENT,
-  `fk_usuario` INT NOT NULL,
-  PRIMARY KEY (`id_socio`, `fk_usuario`),
+  `id_usuario` INT NOT NULL,
+  PRIMARY KEY (`id_socio`, `id_usuario`),
   UNIQUE INDEX `id_socio_UNIQUE` (`id_socio` ASC) VISIBLE,
-  INDEX `fk_socio_usuario1_idx` (`fk_usuario` ASC) VISIBLE,
+  INDEX `fk_socio_usuario1_idx` (`id_usuario` ASC) VISIBLE,
   CONSTRAINT `fk_socio_usuario1`
-    FOREIGN KEY (`fk_usuario`)
-    REFERENCES `MisRutinasApp`.`usuario` (`id_usuario`)
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `MisRutinasApp`.`Usuario` (`id_usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`entrenador`
+-- Table `MisRutinasApp`.`Entrenador`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`entrenador` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Entrenador` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`entrenador` (
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Entrenador` (
   `id_entrenador` INT NOT NULL,
-  `fk_usuario` INT NOT NULL,
-  PRIMARY KEY (`id_entrenador`, `fk_usuario`),
-  INDEX `fk_entrenador_usuario1_idx` (`fk_usuario` ASC) VISIBLE,
+  `id_usuario` INT NOT NULL,
+  PRIMARY KEY (`id_entrenador`, `id_usuario`),
+  INDEX `fk_entrenador_usuario1_idx` (`id_usuario` ASC) VISIBLE,
   CONSTRAINT `fk_entrenador_usuario1`
-    FOREIGN KEY (`fk_usuario`)
-    REFERENCES `MisRutinasApp`.`usuario` (`id_usuario`)
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `MisRutinasApp`.`Usuario` (`id_usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`calendario`
+-- Table `MisRutinasApp`.`Calendario`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`calendario` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Calendario` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`calendario` (
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Calendario` (
   `id_calen` INT NOT NULL AUTO_INCREMENT,
   `hora` TIME NOT NULL,
-  `fecha` DATE NULL,
+  `fecha` DATE NOT NULL,
   PRIMARY KEY (`id_calen`),
   UNIQUE INDEX `id_calen_UNIQUE` (`id_calen` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`clase`
+-- Table `MisRutinasApp`.`Clase`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`clase` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Clase` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`clase` (
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Clase` (
   `id_clase` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
   `precio` DECIMAL NOT NULL,
   `descripcion` VARCHAR(200) NULL,
-  `clasecol` VARCHAR(45) NULL,
   PRIMARY KEY (`id_clase`),
   UNIQUE INDEX `id_clase_UNIQUE` (`id_clase` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`horario`
+-- Table `MisRutinasApp`.`Horario`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`horario` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Horario` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`horario` (
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Horario` (
   `id_hor` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(30) NOT NULL,
   `inicio` TIME NOT NULL,
@@ -137,11 +135,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`dia`
+-- Table `MisRutinasApp`.`Dia`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`dia` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Dia` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`dia` (
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Dia` (
   `id_dia` INT NOT NULL AUTO_INCREMENT,
   `nombre` CHAR(9) NOT NULL,
   PRIMARY KEY (`id_dia`),
@@ -150,112 +148,112 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`turno`
+-- Table `MisRutinasApp`.`Turno`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`turno` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Turno` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`turno` (
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Turno` (
   `id_turno` INT NOT NULL AUTO_INCREMENT,
-  `nombre` CHAR(6) NOT NULL,
-  `fk_dia` INT NOT NULL,
-  `fk_horario` INT NOT NULL,
-  PRIMARY KEY (`id_turno`, `fk_dia`, `fk_horario`),
+  `nombre` VARCHAR(30) NOT NULL,
+  `id_dia` INT NOT NULL,
+  `id_hor` INT NOT NULL,
+  PRIMARY KEY (`id_turno`, `id_dia`, `id_hor`),
   UNIQUE INDEX `id_turno_UNIQUE` (`id_turno` ASC) VISIBLE,
-  INDEX `fk_turno_dia1_idx` (`fk_dia` ASC) VISIBLE,
-  INDEX `fk_turno_horario1_idx` (`fk_horario` ASC) VISIBLE,
+  INDEX `fk_turno_dia1_idx` (`id_dia` ASC) VISIBLE,
+  INDEX `fk_turno_horario1_idx` (`id_hor` ASC) VISIBLE,
   CONSTRAINT `fk_turno_dia1`
-    FOREIGN KEY (`fk_dia`)
-    REFERENCES `MisRutinasApp`.`dia` (`id_dia`)
+    FOREIGN KEY (`id_dia`)
+    REFERENCES `MisRutinasApp`.`Dia` (`id_dia`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_turno_horario1`
-    FOREIGN KEY (`fk_horario`)
-    REFERENCES `MisRutinasApp`.`horario` (`id_hor`)
+    FOREIGN KEY (`id_hor`)
+    REFERENCES `MisRutinasApp`.`Horario` (`id_hor`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`clase_turno`
+-- Table `MisRutinasApp`.`Clase_turno`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`clase_turno` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Clase_turno` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`clase_turno` (
-  `fk_clase` INT NOT NULL,
-  `fk_turno` INT NOT NULL,
-  `fk_entren` INT NOT NULL,
-  `id_clase_turno` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`fk_clase`, `fk_turno`, `fk_entren`, `id_clase_turno`),
-  INDEX `fk_clase_has_turno_turno1_idx` (`fk_turno` ASC) VISIBLE,
-  INDEX `fk_clase_has_turno_clase_idx` (`fk_clase` ASC) VISIBLE,
-  UNIQUE INDEX `id_clase_turno_UNIQUE` (`id_clase_turno` ASC) VISIBLE,
-  INDEX `fk_clase_turno_entrenador1_idx` (`fk_entren` ASC) VISIBLE,
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Clase_turno` (
+  `id_clase_turno` INT NOT NULL AUTO_INCREMENT,
+  `id_clase` INT NOT NULL,
+  `id_turno` INT NOT NULL,
+  `id_entrenador` INT NOT NULL,
+  PRIMARY KEY (`id_clase_turno`, `id_clase`, `id_turno`, `id_entrenador`),
+  INDEX `fk_clase_has_turno_turno1_idx` (`id_turno` ASC) VISIBLE,
+  INDEX `fk_clase_has_turno_clase_idx` (`id_clase` ASC) VISIBLE,
+  INDEX `fk_clase_turno_entrenador1_idx` (`id_entrenador` ASC) VISIBLE,
   CONSTRAINT `fk_clase_has_turno_clase`
-    FOREIGN KEY (`fk_clase`)
-    REFERENCES `MisRutinasApp`.`clase` (`id_clase`)
+    FOREIGN KEY (`id_clase`)
+    REFERENCES `MisRutinasApp`.`Clase` (`id_clase`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_clase_has_turno_turno1`
-    FOREIGN KEY (`fk_turno`)
-    REFERENCES `MisRutinasApp`.`turno` (`id_turno`)
+    FOREIGN KEY (`id_turno`)
+    REFERENCES `MisRutinasApp`.`Turno` (`id_turno`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_clase_turno_entrenador1`
-    FOREIGN KEY (`fk_entren`)
-    REFERENCES `MisRutinasApp`.`entrenador` (`id_entrenador`)
+    FOREIGN KEY (`id_entrenador`)
+    REFERENCES `MisRutinasApp`.`Entrenador` (`id_entrenador`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`socio_clase_turno`
+-- Table `MisRutinasApp`.`Socio_clase_turno`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`socio_clase_turno` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Socio_clase_turno` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`socio_clase_turno` (
-  `id_soc_cla` INT NOT NULL AUTO_INCREMENT,
-  `fk_socio` INT NOT NULL,
-  `fk_clase_turno` INT NOT NULL,
-  PRIMARY KEY (`id_soc_cla`, `fk_socio`, `fk_clase_turno`),
-  UNIQUE INDEX `id_soc_cla_UNIQUE` (`id_soc_cla` ASC) VISIBLE,
-  INDEX `fk_socio_clase_turno_socio1_idx` (`fk_socio` ASC) VISIBLE,
-  INDEX `fk_socio_clase_turno_clase_turno1_idx` (`fk_clase_turno` ASC) VISIBLE,
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Socio_clase_turno` (
+  `id_soc_cla_tur` INT NOT NULL AUTO_INCREMENT,
+  `id_socio` INT NOT NULL,
+  `id_clase_turno` INT NOT NULL,
+  `nombre` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`id_soc_cla_tur`, `id_socio`, `id_clase_turno`),
+  UNIQUE INDEX `id_soc_cla_UNIQUE` (`id_soc_cla_tur` ASC) VISIBLE,
+  INDEX `fk_socio_clase_turno_socio1_idx` (`id_socio` ASC) VISIBLE,
+  INDEX `fk_socio_clase_turno_clase_turno1_idx` (`id_clase_turno` ASC) VISIBLE,
   CONSTRAINT `fk_socio_clase_turno_socio1`
-    FOREIGN KEY (`fk_socio`)
-    REFERENCES `MisRutinasApp`.`socio` (`id_socio`)
+    FOREIGN KEY (`id_socio`)
+    REFERENCES `MisRutinasApp`.`Socio` (`id_socio`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_socio_clase_turno_clase_turno1`
-    FOREIGN KEY (`fk_clase_turno`)
-    REFERENCES `MisRutinasApp`.`clase_turno` (`id_clase_turno`)
+    FOREIGN KEY (`id_clase_turno`)
+    REFERENCES `MisRutinasApp`.`Clase_turno` (`id_clase_turno`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `MisRutinasApp`.`socio_clase_turno_calendario`
+-- Table `MisRutinasApp`.`Soc_clac_tur_cal`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `MisRutinasApp`.`socio_clase_turno_calendario` ;
+DROP TABLE IF EXISTS `MisRutinasApp`.`Soc_clac_tur_cal` ;
 
-CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`socio_clase_turno_calendario` (
-  `id_soc_cla_cal` INT NOT NULL AUTO_INCREMENT,
-  `fk_socio_clase_turno` INT NOT NULL,
-  `fk_calendario` INT NOT NULL,
-  PRIMARY KEY (`id_soc_cla_cal`, `fk_socio_clase_turno`, `fk_calendario`),
-  UNIQUE INDEX `id_soc_cla_cal_UNIQUE` (`id_soc_cla_cal` ASC) VISIBLE,
-  INDEX `fk_socio_clase_turno_calendario_socio_clase_turno1_idx` (`fk_socio_clase_turno` ASC) VISIBLE,
-  INDEX `fk_socio_clase_turno_calendario_calendario1_idx` (`fk_calendario` ASC) VISIBLE,
+CREATE TABLE IF NOT EXISTS `MisRutinasApp`.`Soc_clac_tur_cal` (
+  `id_soc_clac_tur_cal` INT NOT NULL AUTO_INCREMENT,
+  `id_soc_clac_tur` INT NOT NULL,
+  `id_calen` INT NOT NULL,
+  PRIMARY KEY (`id_soc_clac_tur_cal`, `id_soc_clac_tur`, `id_calen`),
+  UNIQUE INDEX `id_soc_cla_cal_UNIQUE` (`id_soc_clac_tur_cal` ASC) VISIBLE,
+  INDEX `fk_socio_clase_turno_calendario_socio_clase_turno1_idx` (`id_soc_clac_tur` ASC) VISIBLE,
+  INDEX `fk_socio_clase_turno_calendario_calendario1_idx` (`id_calen` ASC) VISIBLE,
   CONSTRAINT `fk_socio_clase_turno_calendario_socio_clase_turno1`
-    FOREIGN KEY (`fk_socio_clase_turno`)
-    REFERENCES `MisRutinasApp`.`socio_clase_turno` (`id_soc_cla`)
+    FOREIGN KEY (`id_soc_clac_tur`)
+    REFERENCES `MisRutinasApp`.`Socio_clase_turno` (`id_soc_cla_tur`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_socio_clase_turno_calendario_calendario1`
-    FOREIGN KEY (`fk_calendario`)
-    REFERENCES `MisRutinasApp`.`calendario` (`id_calen`)
+    FOREIGN KEY (`id_calen`)
+    REFERENCES `MisRutinasApp`.`Calendario` (`id_calen`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
