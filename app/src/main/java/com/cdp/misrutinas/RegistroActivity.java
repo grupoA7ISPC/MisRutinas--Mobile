c6841a9b408ed133460471384d88281065487a00
estebanmartins <estebanmartins25@gmail.com>
2023-10-29 10:46:39 -0300

    se sincroniza autentificacion firebase con bd sqlite

--- app/src/main/java/com/cdp/misrutinas/RegistroActivity.java
+++ app/src/main/java/com/cdp/misrutinas/RegistroActivity.java
@@ -1,5 +1,6 @@
 package com.cdp.misrutinas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistroActivity extends AppCompatActivity {
     private CrudCliente crud;
@@ -22,6 +29,7 @@ public class RegistroActivity extends AppCompatActivity {
 
     private Button btnInsertar;
     private TextView textoResultado;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,38 +45,49 @@ public class RegistroActivity extends AppCompatActivity {
         btnInsertar = findViewById(R.id.btnInsertar);
         textoResultado = findViewById(R.id.textoResultado);
 
+        // Inicializa Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
         crud = new CrudCliente(this);
+
 
         btnInsertar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = textUserName.getText().toString();
-                String email = textEmail.getText().toString();
-                String password = textPassword.getText().toString();
-                String nombre = textNombre.getText().toString();
-                String apellido = textApellido.getText().toString();
-                String dni = textDNI.getText().toString();
+                final String username = textUserName.getText().toString();
+                final String email = textEmail.getText().toString();
+                final String password = textPassword.getText().toString();
+                final String nombre = textNombre.getText().toString();
+                final String apellido = textApellido.getText().toString();
+                final String dni = textDNI.getText().toString();
 
-                long id = crud.insertarUsuario(username, email, password, nombre, apellido, dni);
+                // Registra al usuario en Firebase Authentication
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // El usuario se ha registrado con éxito en Firebase
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    // Realiza otras acciones en Firebase si es necesario
 
-                if (id != -1) {
-                    textoResultado.setText("Registro insertado con ID: " + id);
-                } else {
-                    textoResultado.setText("Error al insertar el registro.");
-                }
+                                    // Inserta los datos en tu base de datos local
+                                    long id = crud.insertarUsuario(username, email, password, nombre, apellido, dni);
+
+                                    if (id != -1) {
+                                        // Registro exitoso en Firebase y en la base de datos local
+                                        textoResultado.setText("Registro exitoso. Usuario creado en Firebase y en la base de datos local.");
+                                    } else {
+                                        // Registro exitoso en Firebase, pero error en la base de datos local
+                                        textoResultado.setText("Registro exitoso en Firebase, pero error al insertar el registro en la base de datos local.");
+                                    }
+                                } else {
+                                    // Si el registro falla en Firebase, muestra un mensaje de error
+                                    textoResultado.setText("Error al registrar el usuario en Firebase.");
+                                }
+                            }
+                        });
             }
         });
 
-    }
-
-
-
-    public void btnVolverRegistro(View view){
-        Intent intent=new Intent(RegistroActivity.this,MainActivity.class);
-        startActivity(intent);
-    }
-    public void btnRegistro(View view){
-//        Intent intent=new Intent(RegistroActivity.this,IniciarSesionActivity.class);
-//        startActivity(intent);
     }
 }
